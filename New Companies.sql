4b595dc012be69e9198466b294b8d84a6f9b4a4b
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-03-11 17:07:30 -0500

    Create New Companies.sql

---
+++ New Companies.sql
@@ -0,0 +1,14 @@
+SELECT c.company_code, c.founder, COUNT(DISTINCT l.lead_manager_code), COUNT(DISTINCT s.senior_manager_code), COUNT(DISTINCT m.manager_code), COUNT(DISTINCT e.employee_code)
+FROM Company c, 
+     Lead_Manager l, 
+     Senior_Manager s,
+     Manager m, 
+     Employee e
+WHERE c.company_code = l.company_code
+AND l.company_code = s.company_code
+AND s.company_code = m.company_code
+AND m.company_code = e.company_code
+GROUP BY c.company_code, c.founder
+ORDER BY CAST(SUBSTR(c.company_code,2) AS UNSIGNED);
+
+-- unsigned:insert negative numbers will return error
