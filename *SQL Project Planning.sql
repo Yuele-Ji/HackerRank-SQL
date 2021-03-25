fcb83b0c84c82f69b82d99821e7d143d3127a839
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-03-25 13:57:20 -0400

    Update *SQL Project Planning.sql

--- *SQL Project Planning.sql
+++ *SQL Project Planning.sql
@@ -1,3 +1,4 @@
+--1.
 SELECT Start_Date, MIN(End_Date)
 FROM
 (SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) AS s,
@@ -11,3 +12,17 @@ and choosing the MIN means for the particular start_date,
 we get the closest end date that does not coincide with the start of another task */
 GROUP BY Start_Date
 ORDER BY DATEDIFF(MIN(End_Date), Start_Date), Start_Date
+                                                      
+                               
+--2.
+SELECT T1.Start_Date,T2.End_Date 
+FROM (
+    SELECT Start_Date, ROW_NUMBER() OVER (ORDER BY Start_Date) RN 
+    FROM Projects 
+    WHERE Start_Date NOT IN (SELECT END_Date FROM Projects) ) AS T1
+JOIN (
+    SELECT End_Date, ROW_NUMBER() OVER (ORDER BY End_Date) RN 
+    FROM Projects 
+    WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) AS T2 
+ON T1.RN = T2.RN 
+ORDER BY DATEDIFF(T2.End_Date, T1.Start_Date), T1.Start_Date                                                      
