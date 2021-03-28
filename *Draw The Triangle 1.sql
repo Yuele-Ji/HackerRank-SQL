0a5bea682387b825e3e4b1d68ecf96428176626f
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-03-28 16:56:07 -0400

    Update *Draw The Triangle 1.sql

--- *Draw The Triangle 1.sql
+++ *Draw The Triangle 1.sql
@@ -1,4 +1,4 @@
 SET @number = 21;
-SELECT REPEAT('* ', @number := @number - 1) 
-FROM information_schema.tables 
+SELECT REPEAT('* ', @number := @number - 1) /* := assign value*/
+FROM information_schema.tables /* a built-in table*/
 LIMIT 20;
